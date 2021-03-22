0332f42ebcc32b65f0f97aee113ff28d06db348b
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-03-22 15:23:21 +0100

    Es 1 ok

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@
  * Fase 3 Stampare esito
  */
 
-// Fase 1 
+// Fase 1 Creazione Array
 
 var accounts = ['giuseppe.mandriani@gmail.com', 'serafina90@live.it', 'beppe2008@hotmail.it'];
 console.log(accounts);
@@ -21,15 +21,27 @@ console.table(accounts); //Sotto forma di table in console
 
 var userName = prompt('Inserire indirizzo email').toLowerCase(); //Metodo 1 trasformo direttamente tutto in minuscolo
 // userName = userName.toLowerCase();   // Metodo 2 Sovrascirvo variabile e Trasformo tutto in minuscolo
-console.log(userName);
+console.log('User inserito dall\'utente: ' + userName);
 
-// Fase 3 
+// Fase 3/4 Verifica e Stampa
 
-var userFound = false; //Imposto 
+var userFound = false; //Imposto condizione
 
 for (var i = 0; i < accounts.length; i++){
+    console.log(accounts[i]);
+
     if(accounts[i] === userName) {
         userFound = true;
-
+        break;
     }
+}
+
+if (userFound === true){
+    console.log('Utente trovato, accedi');
+    document.getElementById('check').innerHTML = ('Benvenuto'+ userName);
+} else {
+    console.log('Errore di autenticazione');
+    document.getElementById('check').innerHTML = ('Spiacente, indirizzo email errato');
+    alert('Reinserire indirizzo email');
+
 }
